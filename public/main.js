9b55a020aae6c51c8cbbf3bf7d3aa76c53ad7b68
David Urbina <durbina1991@gmail.com>
2016-04-28 00:48:14 -0700

    More Angular REvisions

--- public/main.js
+++ public/main.js
@@ -2,19 +2,20 @@
 var app = angular.module("myApp", []);
 
 app.controller("mainController", function ($scope) {
-    
+
+
     $scope.transactionList = [
         {
             date : "10/10/2016",
             name : "40 of King Cobra",
-            debit : "$3.99",
+            debit : "4",
             credit : "",
             memo : "Got drunk as fu' in the future."
         },
         {
             date : "12/12/2029",
             name : "Ray Gun",
-            debit : "$49.99",
+            debit : "50",
             credit : "",
             memo : "Weapon for the aliens"
         },
@@ -22,11 +23,13 @@ app.controller("mainController", function ($scope) {
             date : "09/19/2016",
             name : "Treasure Chest",
             debit : "",
-            credit : "$100,000,000.16",
+            credit : "1000000",
             memo : "Found that buried treasure, Doe."
         }
     ];
-    
+
+    $scope.totalCredit = 1000000.00;
+    $scope.totalDebit = 54.00;
 
     $scope.sortBy = function (order) {
         if ($scope.sortOrder === order) {
@@ -45,9 +48,11 @@ app.controller("mainController", function ($scope) {
         console.log($scope.newTransaction);
         if($scope.newTransaction.type === "Credit") {
             newTransaction.credit = $scope.newTransaction.amount;
+            $scope.totalCredit += newTransaction.credit;
             newTransaction.debit = "";
         } else {
             newTransaction.debit = $scope.newTransaction.amount;
+            $scope.totalDebit += newTransaction.debit;
             newTransaction.credit = "";
         }
         $scope.transactionList.push(newTransaction);
